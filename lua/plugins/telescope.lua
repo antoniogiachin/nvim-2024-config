ece67f691f5639744f3b7f8e55768529ab4891cd
antoniogiachin <giachinantonio@outlook.com>
2025-01-21 15:45:56 +0100

    snacks picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,154 +1,156 @@
-return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
-		},
-		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
-	},
-	config = function()
-		local function filenameFirst(_, path)
-			local tail = vim.fs.basename(path)
-			local parent = vim.fs.dirname(path)
-			if parent == "." then
-				return tail
-			end
-			return string.format("%s\t\t%s", tail, parent)
-		end
-		require("telescope").setup({
-			-- You can put your default mappings / updates / etc. in here
-			initial_mode = "insert",
-			selection_strategy = "reset",
-			path_display = { "smart" },
-			color_devicons = true,
-			set_env = { ["COLORTERM"] = "truecolor" },
-			sorting_strategy = nil,
-			layout_strategy = nil,
-			layout_config = {},
-			vimgrep_arguments = {
-				"rg",
-				"--color=never",
-				"--no-heading",
-				"--with-filename",
-				"--line-number",
-				"--column",
-				"--smart-case",
-				-- "--hidden",
-				"--glob=!.git/",
-			},
-			--  All the info you're looking for is in `:help telescope.setup()`
-			--
-			defaults = {
-				mappings = {
-					i = { ["<c-enter>"] = "to_fuzzy_refine" },
-				},
-				file_ignore_patterns = {
-					"%.git/",
-					"node_modules/",
-					"coverage/",
-					"__pycache__/",
-					"%.o",
-					"client/graphql/",
-					"**/generatedTypesAndHooks.ts",
-				},
-				preview = {
-					treesitter = false,
-				},
-			},
-			pickers = {
-				live_grep = {
-					theme = "ivy",
-				},
-
-				grep_string = {
-					theme = "ivy",
-				},
-
-				find_files = {
-					theme = "dropdown",
-					previewer = false,
-					path_display = filenameFirst,
-				},
-
-				buffers = {
-					theme = "dropdown",
-					previewer = false,
-				},
-
-				colorscheme = {
-					enable_preview = true,
-				},
-
-				lsp_dynamic_workspace_symbols = {
-					theme = "ivy",
-				},
-
-				lsp_document_symbols = {
-					theme = "ivy",
-				},
-
-				lsp_references = {
-					theme = "ivy",
-					initial_mode = "normal",
-				},
-
-				lsp_definitions = {
-					theme = "ivy",
-					initial_mode = "normal",
-				},
-
-				lsp_declarations = {
-					theme = "ivy",
-					initial_mode = "normal",
-				},
-
-				lsp_implementations = {
-					theme = "ivy",
-					initial_mode = "normal",
-				},
-			},
-		})
-
-		-- Enable Telescope extensions if they are installed
-		pcall(require("telescope").load_extension, "fzf")
-
-		-- KEYMAPS
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
-		-- Slightly advanced example of overriding default behavior and theme
-		vim.keymap.set("n", "<leader>/", function()
-			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
-
-		-- It's also possible to pass additional configuration options.
-		--  See `:help telescope.builtin.live_grep()` for information about particular keys
-		vim.keymap.set("n", "<leader>s/", function()
-			builtin.live_grep({
-				grep_open_files = true,
-				prompt_title = "Live Grep in Open Files",
-			})
-		end, { desc = "[S]earch [/] in Open Files" })
-
-		-- Shortcut for searching your Neovim configuration files
-		vim.keymap.set("n", "<leader>sn", function()
-			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
-	end,
-}
+-- return {
+-- 	"nvim-telescope/telescope.nvim",
+-- 	branch = "0.1.x",
+-- 	dependencies = {
+-- 		"nvim-lua/plenary.nvim",
+-- 		{
+-- 			"nvim-telescope/telescope-fzf-native.nvim",
+-- 			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+-- 		},
+-- 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+-- 	},
+-- 	config = function()
+-- 		local function filenameFirst(_, path)
+-- 			local tail = vim.fs.basename(path)
+-- 			local parent = vim.fs.dirname(path)
+-- 			if parent == "." then
+-- 				return tail
+-- 			end
+-- 			return string.format("%s\t\t%s", tail, parent)
+-- 		end
+-- 		require("telescope").setup({
+-- 			-- You can put your default mappings / updates / etc. in here
+-- 			initial_mode = "insert",
+-- 			selection_strategy = "reset",
+-- 			path_display = { "smart" },
+-- 			color_devicons = true,
+-- 			set_env = { ["COLORTERM"] = "truecolor" },
+-- 			sorting_strategy = nil,
+-- 			layout_strategy = nil,
+-- 			layout_config = {},
+-- 			vimgrep_arguments = {
+-- 				"rg",
+-- 				"--color=never",
+-- 				"--no-heading",
+-- 				"--with-filename",
+-- 				"--line-number",
+-- 				"--column",
+-- 				"--smart-case",
+-- 				-- "--hidden",
+-- 				"--glob=!.git/",
+-- 			},
+-- 			--  All the info you're looking for is in `:help telescope.setup()`
+-- 			--
+-- 			defaults = {
+-- 				mappings = {
+-- 					i = { ["<c-enter>"] = "to_fuzzy_refine" },
+-- 				},
+-- 				file_ignore_patterns = {
+-- 					"%.git/",
+-- 					"node_modules/",
+-- 					"coverage/",
+-- 					"__pycache__/",
+-- 					"%.o",
+-- 					"client/graphql/",
+-- 					"**/generatedTypesAndHooks.ts",
+-- 				},
+-- 				preview = {
+-- 					treesitter = false,
+-- 				},
+-- 			},
+-- 			pickers = {
+-- 				live_grep = {
+-- 					theme = "ivy",
+-- 				},
+--
+-- 				grep_string = {
+-- 					theme = "ivy",
+-- 				},
+--
+-- 				find_files = {
+-- 					theme = "dropdown",
+-- 					previewer = false,
+-- 					path_display = filenameFirst,
+-- 				},
+--
+-- 				buffers = {
+-- 					theme = "dropdown",
+-- 					previewer = false,
+-- 				},
+--
+-- 				colorscheme = {
+-- 					enable_preview = true,
+-- 				},
+--
+-- 				lsp_dynamic_workspace_symbols = {
+-- 					theme = "ivy",
+-- 				},
+--
+-- 				lsp_document_symbols = {
+-- 					theme = "ivy",
+-- 				},
+--
+-- 				lsp_references = {
+-- 					theme = "ivy",
+-- 					initial_mode = "normal",
+-- 				},
+--
+-- 				lsp_definitions = {
+-- 					theme = "ivy",
+-- 					initial_mode = "normal",
+-- 				},
+--
+-- 				lsp_declarations = {
+-- 					theme = "ivy",
+-- 					initial_mode = "normal",
+-- 				},
+--
+-- 				lsp_implementations = {
+-- 					theme = "ivy",
+-- 					initial_mode = "normal",
+-- 				},
+-- 			},
+-- 		})
+--
+-- 		-- Enable Telescope extensions if they are installed
+-- 		pcall(require("telescope").load_extension, "fzf")
+--
+-- 		-- KEYMAPS
+-- 		local builtin = require("telescope.builtin")
+-- 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+-- 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+-- 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+-- 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+-- 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+-- 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+-- 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+-- 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+-- 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+-- 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+--
+-- 		-- Slightly advanced example of overriding default behavior and theme
+-- 		vim.keymap.set("n", "<leader>/", function()
+-- 			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+-- 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+-- 				winblend = 10,
+-- 				previewer = false,
+-- 			}))
+-- 		end, { desc = "[/] Fuzzily search in current buffer" })
+--
+-- 		-- It's also possible to pass additional configuration options.
+-- 		--  See `:help telescope.builtin.live_grep()` for information about particular keys
+-- 		vim.keymap.set("n", "<leader>s/", function()
+-- 			builtin.live_grep({
+-- 				grep_open_files = true,
+-- 				prompt_title = "Live Grep in Open Files",
+-- 			})
+-- 		end, { desc = "[S]earch [/] in Open Files" })
+--
+-- 		-- Shortcut for searching your Neovim configuration files
+-- 		vim.keymap.set("n", "<leader>sn", function()
+-- 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+-- 		end, { desc = "[S]earch [N]eovim files" })
+-- 	end,
+-- }
+--
+return {}
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,41 +1,24 @@
 return {
-	{
 	"folke/trouble.nvim",
-	opts = {}, -- for default options, refer to the configuration section for custom setup.
-	cmd = "Trouble",
-	keys = {
-		{
-			"<leader>xx",
-			"<cmd>Trouble diagnostics<cr>",
-			desc = "diagnostics (trouble)",
-		},
-		-- {
-		--   '<leader>xx',
-		--   '<cmd>trouble diagnostics toggle filter.buf=0<cr>',
-		--   desc = 'buffer diagnostics (trouble)',
-		-- },
-		-- {
-		--   '<leader>cs',
-		--   '<cmd>trouble symbols toggle focus=false<cr>',
-		--   desc = 'symbols (trouble)',
-		-- },
-		-- {
-		--   '<leader>cl',
-		--   '<cmd>trouble lsp toggle focus=false win.position=right<cr>',
-		--   desc = 'lsp definitions / references / ... (trouble)',
-		-- },
-		-- {
-		--   '<leader>xl',
-		--   '<cmd>trouble loclist toggle<cr>',
-		--   desc = 'location list (trouble)',
-		-- },
-		-- {
-		--   '<leader>xq',
-		--   '<cmd>trouble qflist toggle<cr>',
-		--   desc = 'quickfix list (trouble)',
-		-- },
+	optional = true,
+	specs = {
+		"folke/snacks.nvim",
+		opts = function(_, opts)
+			return vim.tbl_deep_extend("force", opts or {}, {
+				picker = {
+					actions = require("trouble.sources.snacks").actions,
+					win = {
+						input = {
+							keys = {
+								["<c-t>"] = {
+									"trouble_open",
+									mode = { "n", "i" },
+								},
+							},
+						},
+					},
+				},
+			})
+		end,
 	},
-	},
-	{'nvim-tree/nvim-web-devicons'
-	}
 }
